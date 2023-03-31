445ea97782de461661d35a2e791bc42cff6abc83
Eldad Danladi <eldaddanladi@gmail.com>
2023-03-31 12:47:27 -0600

    modification: renamed files and classes

--- src/main/java/com/example/apexlater/ApexLaterController.java
+++ src/main/java/com/example/apexlater/ApexLaterController.java
@@ -3,7 +3,7 @@ package com.example.apexlater;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ApexLaterController {
     @FXML
     private Label welcomeText;
 
