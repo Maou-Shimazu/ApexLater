6d5db870c169330047a525b834c031ed22a7d6d7
Eldad Danladi <eldaddanladi@gmail.com>
2023-04-06 04:17:55 -0600

    modifications: In the process of completing the classes for deserialization of json. Switched to a singular api

--- src/main/java/com/example/apexlater/Information.java
+++ src/main/java/com/example/apexlater/Information.java
@@ -1,21 +1,78 @@
 package com.example.apexlater;
 
 public class Information {
-    Data data;
-    public Information(Data data) {
-        this.data = data;
-    }
+    Global global;
+    Rank rank;
+    Arena arena;
+    Battlepass battlepass;
+    private static int internalParsingVersion;
+    Badges badges;
+    Realtime realtime;
+    Legends legends; // add everything for the legends https://stackoverflow.com/questions/10292514/how-to-deserialize-a-json-array-of-objects-using-gson-library
+    Mozambique_Internal mozambiquehere_internal;
+    ALS als; // add alias for this with the lib
+    Total total;
 
-    private static class Data {
-        PlatformInfo platformInfo;
-
-        private static class PlatformInfo {
-            private String platformSlug;
-            private String platformUserId;
-            private String platformUserHandle;
-            private String platformUserIdentifier;
-            private String avatarUrl;
-            private String additionalParameters;
+    private static class Global {
+        private static String name;
+        private static int uid;
+        private static String avatar;
+        private static String platform;
+        private static int level;
+        private static int toNextLevelPercent;
+        private static int internalUpdateCount;
+        Bans bans;
+        private static class Bans {
+            private static boolean isActive;
+            private static int remainingSeconds;
+            private static String last_banReason;
         }
     }
+    private static class Rank {
+        private static int rankScore;
+        private static String rankName;
+        private static int rankDiv;
+        private static int ladderPosPlatform;
+        private static String rankImg;
+        private static String rankedSeason;
+    }
+    private static class Arena {}
+    private static class Battlepass {
+        private static String level;
+        History history;
+        private static class History {
+            private static int season1;
+            private static int season2;
+            private static int season3;
+            private static int season4;
+            private static int season5;
+            private static int season6;
+            private static int season7;
+            private static int season8;
+            private static int season9;
+            private static int season10;
+            private static int season11;
+            private static int season12;
+            private static int season13;
+            private static int season14;
+        }
+    }
+    private static class Badges {}
+    private static class Realtime {
+        private static String lobbyState;
+        private static int isOnline;
+        private static int isInGame;
+        private static int canJoin;
+        private static int partyFull;
+        private static String selectedLegend;
+        private static String currentState;
+        private static int currentStateSinceTimestamp;
+        private static String currentStateAsText;
+
+    }
+    private static class Legends {}
+    private static class Mozambique_Internal{}
+    private static class ALS {}
+    private static class Total {}
+
 }
